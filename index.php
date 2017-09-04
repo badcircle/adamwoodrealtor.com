c7b7f3b90c1343359aae355422c6c2b7f0bae942
Adam Wood <wood@badcircle.com>
2017-09-04 15:25:04 -0500

    initial design and layout

---
+++ index.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation for Sites</title>
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  <body>
+    
+  <div class="grid-container">
+      <div class="grid-x">
+          <div class="cell shrink">
+            <img src="img/cropped-blurealtylogo.png" class="logo" alt="">
+          </div>
+          <div class="cell auto">
+            <ul class="menu align-right">
+              <li><a href="#" class="button small">CLIENT LOGIN</a></li>
+            </ul>
+          </div>
+      </div>
+      <div class="grid-x">
+          <div class="cell small-10 small-offset-1 navigation">
+            <ul class="menu simple align-center expanded">
+              <li class="left"><a href="#" class="active left">About</a></li>
+              <li><a href="#">Featured Listings</a></li>
+              <li><a href="#">Buy</a></li>
+              <li><a href="#">Sell</a></li>
+              <li><a href="#">Home Valuation</a></li>
+              <li class="right"><a href="#" class="right">Contact</a></li>
+            </ul>
+          </div>
+      </div>
+  </div>
+
+  <div class="grid-x" id="main">
+    <div class="cell">
+      
+      <div class="grid-container">
+        <div class="grid-x">
+          <div class="cell small-10 small-offset-1">
+            <h1>Buying or selling your next home doesn't have to be hard or confusing. Let's get started.</h1>
+            <h5>Serving Lubbock, TX and surrounding areas</h5>
+
+            <div class="grid-x action" data-equalizer>
+              <div class="cell large-8" data-equalizer-watch data-equalize-on="medium">
+                <h4>Adam Wood</h4>
+                <p>(806) 777-9831 &middot; <a href="mailto:awood@theblurealty.com?subject=Realtor Services">awood@theblurealty.com</a></p>
+              </div>
+              <div class="cell small-12 large-4 action-button" data-equalizer-watch>
+                <a href="#" class="button large">Get Started &rarr;</a>
+              </div>
+            </div>
+
+            <img src="img/realtor.png" class="realtor" alt="REALTOR" />
+
+          </div>
+        </div>
+      </div>
+      
+      <img src="img/bw-cropped-fs8.png" class="headshot" alt="Headshot of Adam">
+
+    </div>
+  </div>
+
+  <div class="grid-container" id="footer">
+    <div class="grid-x">
+      <div class="cell large-2 medium-11 small-11 small-offset-1">
+        <h6>Office Location</h6>
+        <p>
+          Blu Realty<br />
+          1506 Texas Ave.<br />
+          Lubbock, TX 79401
+        </p>
+      </div>
+      <div class="cell large-3 medium-5 small-offset-1 small-11">
+        <h6>Associations</h6>
+        <p>Adam Wood is a member of the <a href="http://lubbockrealtors.com">LAR</a> <em>Lubbock Association of Realtors</em>, <a href="http://texasrealestate.com">TAR</a> <em>Texas Association of Realtors</em>, and <a href="http://realtor.com">NAR</a> <em>National Association of Realtors</em>.</p>
+        <p>Proud Associate of <a href="http://theblurealty.com">Blu Realty</a></p>
+      </div>
+      <div class="cell large-3 medium-5 small-offset-1 small-11">
+        <h6>Information</h6>
+        <ul>
+          <li><a href="#">TREC Information About Brokerage Service</a></li>
+          <li><a href="#">TREC Consumer Protection Notice</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/what-input.js"></script>
+    <script src="js/foundation.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
