b8743b7ff2f9b648d2398442d03e3e3e30e26e39
Adam Wood <wood@badcircle.com>
2017-09-04 18:41:26 -0500

    stopping for the day

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Foundation for Sites</title>
+    <title>Adam Wood, REALTOR&reg;</title>
     <link rel="stylesheet" href="css/app.css">
   </head>
   <body>
+  <div class="layer">
     
   <div class="grid-container">
       <div class="grid-x">
@@ -16,7 +17,7 @@
           </div>
           <div class="cell auto">
             <ul class="menu align-right">
-              <li><a href="#" class="button small">CLIENT LOGIN</a></li>
+              <li><a href="#" class="button small client-login">CLIENT LOGIN</a></li>
             </ul>
           </div>
       </div>
@@ -40,7 +41,7 @@
       <div class="grid-container">
         <div class="grid-x">
           <div class="cell small-10 small-offset-1">
-            <h1>Buying or selling your next home doesn't have to be hard or confusing. Let's get started.</h1>
+            <h1>Buying or selling your next home doesn't have to be a hassle. Let's get started.</h1>
             <h5>Serving Lubbock, TX and surrounding areas</h5>
 
             <div class="grid-x action" data-equalizer>
@@ -64,9 +65,44 @@
     </div>
   </div>
 
+  <div class="grid-x" id="newsletter">
+    <div class="cell">
+      <div class="grid-container">
+        <div class="grid-x">
+          <div class="cell small-10 small-offset-1">
+            <h5>Subscribe to Adam's Quarterly Newsletter</h5>
+            <p>Curated articles, DIY projects, tips and tricks, recipes and other ideas for homeowners, published quarterly.</p>
+            <!-- Begin MailChimp Signup Form -->
+            <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+            <style type="text/css">
+              #mc_embed_signup{width:100%;}
+              /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+                We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+            </style>
+            <div id="mc_embed_signup">
+            <form action="//texasnursesconference.us1.list-manage.com/subscribe/post?u=67343725b832cba5ed29575aa&amp;id=256a708dbf" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+                  <div class="grid-x">
+                    <div class="cell small-12 medium-3"><input type="text" value="" name="FNAME" class="" id="mce-FNAME" placeholder="First Name" /></div>
+                    <div class="cell small-12 medium-3 medium-offset-1"><input type="text" value="" name="LNAME" class="" id="mce-LNAME" placeholder="Last Name" /></div>
+                    <div class="cell small-12 medium-4 medium-offset-1"><input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email Address" required /></div>
+                  </div>
+                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_67343725b832cba5ed29575aa_256a708dbf" tabindex="-1" value=""></div>
+                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                </div>
+            </form>
+            </div>
+            <!--End mc_embed_signup-->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <div class="grid-container" id="footer">
     <div class="grid-x">
-      <div class="cell large-2 medium-11 small-11 small-offset-1">
+      <div class="cell large-2 medium-11 small-10 small-offset-1">
         <h6>Office Location</h6>
         <p>
           Blu Realty<br />
@@ -74,12 +110,19 @@
           Lubbock, TX 79401
         </p>
       </div>
-      <div class="cell large-3 medium-5 small-offset-1 small-11">
+      <div class="cell large-3 medium-5 small-offset-1 small-10">
         <h6>Associations</h6>
-        <p>Adam Wood is a member of the <a href="http://lubbockrealtors.com">LAR</a> <em>Lubbock Association of Realtors</em>, <a href="http://texasrealestate.com">TAR</a> <em>Texas Association of Realtors</em>, and <a href="http://realtor.com">NAR</a> <em>National Association of Realtors</em>.</p>
+        <div class="grid-x">
+          <div class="cell auto">
+            <p>Adam Wood is a member of the <a href="http://lubbockrealtors.com">LAR</a> <em>Lubbock Association of Realtors</em>, <a href="http://texasrealestate.com">TAR</a> <em>Texas Association of Realtors</em>, and <a href="http://realtor.com">NAR</a> <em>National Association of Realtors</em>.</p>
+          </div>
+          <div class="cell shrink">
+            <img src="img/realtor-bw-fs8.png" alt="" style="width: 30px; margin-left: 1rem;" />
+          </div>
+        </div>
         <p>Proud Associate of <a href="http://theblurealty.com">Blu Realty</a></p>
       </div>
-      <div class="cell large-3 medium-5 small-offset-1 small-11">
+      <div class="cell large-3 medium-5 small-offset-1 small-10">
         <h6>Information</h6>
         <ul>
           <li><a href="#">TREC Information About Brokerage Service</a></li>
@@ -87,6 +130,8 @@
         </ul>
       </div>
     </div>
+  </div>
+
   </div>
 
     <script src="js/jquery.js"></script>
