d56915eb13266e9d32891b93aee5ffc8f1156098
Adam Wood <wood@badcircle.com>
2017-09-10 10:50:18 -0500

    newsletter changes

---
+++ newsletter.php
@@ -0,0 +1,34 @@
+<div class="grid-x" id="newsletter">
+<div class="cell">
+    <div class="grid-container">
+    <div class="grid-x">
+        <div class="cell small-10 small-offset-1">
+        <h5>Subscribe to Adam's Quarterly Newsletter</h5>
+        <p class="subline">Curated articles, local spotlight, DIY projects, tips and tricks, recipes and other ideas for homeowners, published quarterly.</p>
+        <!-- Begin MailChimp Signup Form -->
+        <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+        <style type="text/css">
+            #mc_embed_signup{width:100%;}
+            /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+            We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+        </style>
+        <div id="mc_embed_signupß">
+        <form action="//texasnursesconference.us1.list-manage.com/subscribe/post?u=67343725b832cba5ed29575aa&amp;id=256a708dbf" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+                <div class="grid-x grid-margin-x">
+                    <div class="cell small-12 medium-6 large-3 auto"><input type="text" value="" name="FNAME" class="" id="mce-FNAME" placeholder="First Name" /></div>
+                    <div class="cell small-12 medium-6 large-3 auto"><input type="text" value="" name="LNAME" class="" id="mce-LNAME" placeholder="Last Name" /></div>
+                    <div class="cell medium-12 large-6"><input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email Address" required /></div>
+                </div>
+            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_67343725b832cba5ed29575aa_256a708dbf" tabindex="-1" value=""></div>
+            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+            </div>
+        </form>
+        </div>
+        <!--End mc_embed_signup-->
+        </div>
+    </div>
+    </div>
+</div>
+</div>
