32eb2cd70b5118aad058cab80ad6246bacb490db
Adam Wood <wood@badcircle.com>
2017-09-12 22:14:16 -0500

    buy page more fleshed out

--- buy.php
+++ buy.php
@@ -12,17 +12,70 @@
     
   <?php include 'header.php'; ?>
 
-  <div class="grid-x" id="main">
+  <div class="grid-x bare" id="main">
     <div class="cell">
       
-      <div class="grid-container">
-        <div class="grid-x">
-          <div class="cell small-10 small-offset-1">
-            <h1>What are you looking for?</h1>
-            <h5 class="subline">Serving Lubbock, TX and surrounding areas</h5>
+      <div class="grid-container" id="polaroids">
 
+        <div class="grid-x elevated">
+          <div class="cell small-10 small-offset-1">
+            <h1>Your dream home, just clicks away.</h1>
           </div>
         </div>
+
+        <div class="grid-x" id="form_buy" style="margin-top: 3rem;">
+          <div class="cell medium-8 medium-offset-2 small-10 small-offset-1" id="form_content">
+            <form action="" method="POST" data-abide novalidate>
+              
+              <div class="grid-x grid-margin-x" style="margin-top: 1rem;">
+                <div class="cell small-12 large-6">
+                  <label for="">Your Name<br />
+                  <input type="text" required name="name" /></label>
+                </div>
+
+                <div class="cell small-12 large-6">
+                  <label for="">Phone Number<br />
+                  <input type="text" required name="phone" /></label>
+                </div>
+
+                <div class="cell small-12">
+                  <label for="">Email<br />
+                  <input type="text" required name="email" /></label>
+                </div>
+
+                <div class="cell small-12">
+                  <label for="">Tell me about yourself, your family, and some features of your next home<br />
+                  <textarea name="comments" id="comments" cols="30" rows="5"></textarea></label>
+                </div>
+
+              </div>
+
+              <div class="grid-x grid-margin-x">
+                <div class="cell small-12"><span class="label_group">Preferred method of contact:</span></div>
+                <div class="cell large-4 small-12"><label for="communicate_phone"><input type="radio" name="communication" value="Phone" id="communicate_phone"> Phone</label></div>
+                <div class="cell large-4 small-12"><label for="communicate_text"><input type="radio" name="communication" value="Text (SMS)" id="communicate_text"> Text (SMS)</label></div>
+                <div class="cell large-4 small-12"><label for="communicate_email"><input type="radio" name="communication" value="Email" id="communicate_email"> Email</label></div>
+              </div>
+
+              <div class="grid-x grid-margin-x" style="margin-top: 1rem;">
+                <div class="cell small-12"><span class="label_group">When are you expecting to purchase?</span></div>
+                <div class="cell small-12"><label for="purchase_days"><input type="radio" name="moving" value="days" id="purchase_days">in the next few days</label></div>
+                <div class="cell small-12"><label for="purchase_month"><input type="radio" name="moving" value="month" id="purchase_month"> within the month or next few months</label></div>
+                <div class="cell small-12"><label for="purchase_6months"><input type="radio" name="moving" value="6months" id="purchase_6months"> within the next 6 months</label></div>
+              </div>
+
+              <div class="grid-x" style="margin-top: 1rem;">
+                <div class="cell small-12">
+                  <input class="button large" type="submit" value="Submit" style="margin-bottom: 0;" />
+                </div>
+              </div>
+                
+            </form>
+          </div>
+        </div>
+
+        <img src="img/bg_houses/polaroids-1.jpg" class="polaroid polaroid-1" alt="" >
+        <img src="img/bg_houses/polaroids-2.jpg" class="polaroid polaroid-2" alt="" >
       </div>
 
     </div>
--- newsletter.php
+++ newsletter.php
@@ -16,8 +16,8 @@
         <form action="//texasnursesconference.us1.list-manage.com/subscribe/post?u=67343725b832cba5ed29575aa&amp;id=256a708dbf" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
             <div id="mc_embed_signup_scroll">
                 <div class="grid-x grid-margin-x">
-                    <div class="cell small-12 medium-6 large-3 auto"><input type="text" value="" name="FNAME" class="" id="mce-FNAME" placeholder="First Name" /></div>
-                    <div class="cell small-12 medium-6 large-3 auto"><input type="text" value="" name="LNAME" class="" id="mce-LNAME" placeholder="Last Name" /></div>
+                    <div class="cell small-8 medium-6 large-3"><input type="text" value="" name="FNAME" class="" id="mce-FNAME" placeholder="First Name" /></div>
+                    <div class="cell small-8 medium-6 large-3"><input type="text" value="" name="LNAME" class="" id="mce-LNAME" placeholder="Last Name" /></div>
                     <div class="cell medium-12 large-6"><input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email Address" required /></div>
                 </div>
             <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
